20832f61859288139fcb6348ad5e0b892f71bfcd
Dominic Gagliardi <35349713+DomMacgyver@users.noreply.github.com>
2018-11-21 15:04:00 -0500

    Add files via upload

---
+++ Functions.h
@@ -0,0 +1,167 @@
+#ifndef Functions_h
+#define Functions_h
+#define swap(a, b) { double t = a; a = b; b = t; }
+struct vector
+{
+  double x;
+  double y;
+  double z;
+};
+typedef struct vector Vector;
+void printVector(Vector &vec)
+{
+  Serial.println("");
+  Serial.print("X: ");
+  Serial.print(vec.x, 5);
+  Serial.print("; Y: ");
+  Serial.print(vec.y, 5);
+  Serial.print("; Z: ");
+  Serial.print(vec.z, 5);
+  Serial.println("");
+}
+struct sphere
+{
+  double r;
+  double theta;
+  double azimuth;
+};
+typedef struct sphere Sphere;
+struct servoGeometry
+{
+  //coordinates of a vector starting at the center of the axis of rotation of the servo controlling pitch(m1) and ending at the sensor where measurments are taken from
+  double pitch_x;
+  double pitch_y;
+  double pitch_z;
+  //coordinates of a vector starting at the center of the axis of rotation of the servo controlling yaw(m2) and ending at the center of the axis of rotation of the servo controlling pitch
+  double yaw_x;
+  double yaw_y;
+  double yaw_z;
+  //the coordinates of the center of the axis of rotation of the servo controlling yaw relative to space
+  double x;
+  double y;
+  double z;
+};
+typedef struct servoGeometry ServoGeometry;
+
+void calculateOffset(ServoGeometry &geo, double theta, double azimuth, vector &offsets)
+{
+  theta *= -1;
+  theta += (HALF_PI);
+  azimuth -= HALF_PI;
+
+  //https://stackoverflow.com/questions/14607640/rotating-a-vector-in-3d-space
+  //calculate the new vector of m1 after rotating to the given theta
+  //uses a rotaion matrix rotating about the x-axis
+  double tempx = geo.pitch_x;
+  double tempy = geo.pitch_y * cos(theta) - geo.pitch_z * sin(theta);
+  double tempz = geo.pitch_y * sin(theta) + geo.pitch_z * cos(theta);
+  //combine vectors
+  tempx += geo.yaw_x;
+  tempy += geo.yaw_y;
+  tempz += geo.yaw_z;
+  //calculate the new vector of m2 after rotating to the given azimuth
+  //uses a rotaion matrix rotating about the z-axis
+  double temp = tempx * cos(azimuth) - tempy * sin(azimuth);
+  double temp2 = tempx * sin(azimuth) - tempy * cos(azimuth);
+  tempx = temp;
+  tempy = temp2;
+  //add hard offsets from reference frame and store it to the vector offsets
+  offsets.x = (tempx + geo.x);
+  offsets.y = tempy + geo.y;
+  offsets.z = (tempz + geo.z);
+}
+vector sphereToVector(double r, double theta, double azimuth, vector &offsets)
+{
+  Vector data;
+  theta *= -1;
+  theta += (HALF_PI);
+  azimuth -= HALF_PI;
+  double temp1 = sin(theta) * r;
+  data.x = temp1 * cos(azimuth) + offsets.x;
+  data.y = temp1 * sin(azimuth) + offsets.z;
+  data.z = r * cos(theta) + offsets.y;
+  return data;
+}
+void printSphereToVector(double r, double theta, double azimuth, vector &offsets)
+{
+  theta *= -1;
+  theta += (HALF_PI);
+  azimuth -= HALF_PI;
+  double temp1 = sin(theta) * r;
+  double xtemp = temp1 * cos(azimuth) + offsets.x; //converet spherical coordinates to parametric coordintes, https://en.wikipedia.org/wiki/Spherical_coordinate_system#Cartesian_coordinates
+  double ytemp = temp1 * sin(azimuth) + offsets.y;
+  double ztemp = r * cos(theta) + offsets.z;
+  Serial.print(xtemp, 5);
+  Serial.print(", ");
+  Serial.print(ytemp, 5);
+  Serial.print(", ");
+  Serial.print(ztemp, 5);
+
+}
+void printVectorDifference(vector &offsets, vector &signal)
+{
+  double xtemp = -1 * (signal.x - offsets.x);
+  double ytemp = (signal.y);
+  double ztemp = -1 * (signal.z - offsets.z);
+  Serial.print(xtemp, 5);
+  Serial.print(", ");
+  Serial.print(ztemp, 5);
+  Serial.print(", ");
+  Serial.print(ytemp, 5);
+}
+void lsrl(double x[], double y[], int dim, double values[])
+{
+  double b = 0.0;
+  double d = 0.0;
+  for (int i = 0; i < dim; i++)
+  {
+    b += x[i];
+    d += x[i] * x[i];
+  }
+  double det = 1.0;
+  double a = (double)dim;
+  det /= (a * d - b * b);
+  swap(a, d);
+
+  a *= det;
+  b *= -1 * det;
+  d *= det;
+
+  double y0 = 0.0;
+  double y1 = 0.0;
+  for (int i = 0; i < dim; i++)
+  {
+    y0 += y[i];
+    y1 += y[i] * x[i];
+  }
+
+  double b0 = a * y0;
+  b0 += b * y1;
+  double b1 = b * y0;
+  b1 += d * y1;
+
+  double y_mean = y0;
+  y_mean /= dim;
+  double error = 0.0;
+  double sst = 0.0;
+  for (int i = 0; i < dim; i++)
+  {
+    double val = b1 * x[i];
+    val += b0;
+    val -= y[i];
+    val *= val;
+    error += val;
+
+    val = y[i] - y_mean;
+    val *= val;
+    sst += val;
+  }
+  double r = 1 - (error / sst);
+
+  values[0] = b0;
+  values[1] = b1;
+  values[2] = r;
+
+}
+
+#endif
---
+++ Ultrasonic_Room_Scan_11_19_18.ino
@@ -0,0 +1,251 @@
+#include "Functions.h"
+
+
+#include "MeccaBrain.h"
+MeccaBrain servo(5);//servo data pin is connected to digital pin 5
+
+
+int row;
+int x;
+
+const unsigned int TRIG_PIN = 7;
+const unsigned int ECHO_PIN = 6;
+char receivedChar;
+
+// calibration
+// If you have already determined the coefficents for the duration to distance equation, set calibration to false and set the coeffiecnmts to that value
+bool calibration = false;//if this is false you can declare offset and slope as const
+//the equation: y = mx + b, distance = slope * durtion + offset
+double offset = 1.101;//the intercept from the least squares regression analysis
+double slope = 0.0342 / 2.0;//the slope from the least squares regression analysis, divided by 2.0 because the duration is to the object and back but distance to the object would only be time to get there or back.
+
+//room scan variables
+int pstart = 60;
+int pend = 120;
+int ystart = 60;
+int yend = 120;
+int pstep = 5;
+int ystep = 5;
+//scanner variables(Scanner setup geometry)
+//{pitch_x, pitch_y, pitch_z, yaw_x, yaw_y, yaw_z, x, y, z}
+servoGeometry geo1 = { -0.48, 0.52, 6.63, 0.48, 0.0, 7.87, 0.0, 0.0, 6.83};
+servoGeometry geo2 = geo1;
+
+//functions
+unsigned long analog_read()//fucntion to read in vlues from the ultrasonic sensor, if you are using a different sensor than the HY-SRF05 sensor, replace what is in this function
+{
+  //trigger the ultrasonic sensor
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  //recive duration from the sensor
+  unsigned long duration = pulseIn(ECHO_PIN, HIGH);
+  return duration;
+}
+
+void calibrate(double start, double step, unsigned int size, unsigned int sample_size)
+{
+  //initialize arrays
+  double darr[size];// array of pre-measured distances
+  double val[size];// array of average distance values from the sensor
+  double durations[size]; //array of average durations, in ms
+
+  // initialize the array of pre-measured distances
+  for (unsigned int i = 0; i < size; i++)
+  {
+    darr[i] = start + step * i;
+  }
+
+
+  for (unsigned int position = 0; position < size; position++)
+  {
+    bool newData = false;//reset newData flag
+    Serial.println("Waiting for input to advance");
+    Serial.println("Next measurment distance is ");
+    Serial.println(darr[position], 5);
+    while (!newData)//wait for user to enter 'a' into the serial monitor before running for this distance; done for each distance measurment
+    {
+      if (Serial.available() > 0)
+      {
+        receivedChar = Serial.read();
+        if (receivedChar == 'a')
+        {
+          newData = true;
+          receivedChar = ' ';
+        }
+      }
+    }
+
+    unsigned long total = 0;//initialize total
+
+    Serial.println("Ready");//acknowledges that input has been recieved
+    delay(1000);
+    Serial.println("Go");
+
+    //takes an average of the values collected for this measurment distance
+    for (unsigned int num = 0; num < sample_size; num++)
+    {
+
+      unsigned long duration = analog_read();
+      duration /= 2; //divided by 2 because the duration is to the object and back but distance to the object would only be time to get there or back.
+
+      if (duration <= 1 || duration >= 200000)
+      {
+        num -= 1;//don't count this run if the value doesn't make sense
+      }
+      else
+      {
+        Serial.println(num, DEC);//print the current iterarion
+        total += duration;// add the duration to the total
+        delay(20);
+      }
+    }
+    Serial.println("Done");
+
+    double average = total / sample_size * 1.0;// compute the average
+    durations[position] = average;// add this average durtion to the array
+    double distance = average * 0.0342 + 1.101;//calculate the predicted distance
+    val[position] = distance; // store the predicted distance to the array
+  }
+
+  //print the data (to 5 decimal places) as three(3) columns (measured on left, sensor in the middle, and durations on right) sperated by commas
+  Serial.println();
+  for (unsigned int count = 0; count < size; count++)
+  {
+    Serial.print(darr[count], 3);
+    Serial.print(", ");
+    Serial.print(val[count], 5);
+    Serial.print(", ");
+    Serial.print(durations[count], 5);
+    Serial.println();
+  }
+  //helpful site for viewing: http://www.alcula.com/calculators/statistics/scatter-plot/
+  //values can be broken up by pasting into excel and using the text splitter
+
+  double line_vals[3];
+  lsrl(durations, darr, size, line_vals);
+  Serial.println();//print a space
+
+  //print the least squares regression line coefficents and correlation coefficent (R Squared), to 7 decimal places
+  Serial.print("A0(Intercept): ");
+  Serial.print(line_vals[0], 7);
+  Serial.print(", A1(slope): ");
+  Serial.print(line_vals[1], 7);
+  Serial.print(", R Squared: ");
+  Serial.print(line_vals[2], 7);
+  Serial.println();
+  //print equation
+  Serial.println("New Equation:");
+  Serial.println();
+  Serial.print("distance = duration / 2.0 * ");
+  Serial.print(line_vals[1], 4);
+  Serial.print(" + ");
+  Serial.print(line_vals[0], 4);
+  Serial.println();
+  offset = line_vals[0];
+  slope = line_vals[1];
+}
+
+//end functions
+void setup() {
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
+  Serial.println("Initialized"); //comment out this line if you are going to use the seril plotter
+
+  delay(500);
+  if (calibration)
+  {
+
+    //if coefficents have not been determined, set these values
+    const double start = 4.0; //distance to start the interval from
+    const double interval = 1.5; // the distance(cm) between each measurement
+    const unsigned int size = 8;// number of data points that will be collected for calibration
+    const unsigned int sample_size = 10; //the number of samples to take for each average; larger numbers for more accuracy, smaller numbers for faster run times
+
+    calibrate(start, interval, size, sample_size);
+
+  }
+  for (int iter = 0; iter < 20; iter++)
+  {
+    servo.setServoPosition(0, map(ystart, 0, 180, 0x18, 0xE8));
+    servo.setServoPosition(1, map(pstart, 0, 180, 0x18, 0xE8));
+    servo.communicate();//initiate servo communication
+
+    delay(15);
+  }
+}
+
+
+
+void loop() {
+
+  Serial.println("");
+  Serial.print("[");
+  for (int pitch = pstart; pitch < pend; pitch += pstep)
+  {
+    double theta = pitch * DEG_TO_RAD;
+    servo.setServoPosition(1, map(pitch, 180, 0, 0x18, 0xE8));
+
+    servo.communicate();//initiate servo communication
+    delay(100);
+    Serial.println("");
+    for (int yaw = ystart; yaw < yend; yaw += ystep)
+    {
+      double azimuth = yaw * DEG_TO_RAD;
+      servo.setServoPosition(0, map(yaw, 0, 180, 0x18, 0xE8));
+      //servo.setServoPosition(1, map(pitch, 0, 180, 0x18, 0xE8));
+
+      servo.communicate();//initiate servo communication
+      if (yaw == ystart)
+      {
+        delay(1000);
+      }
+      delay(100);
+      Vector v_offsets;
+      calculateOffset(geo1, theta, azimuth, v_offsets);
+      /*Serial.println("");
+      Serial.println("");
+      Serial.print("Pitch: ");
+      Serial.print(pitch, DEC);
+      Serial.print(", Yaw: ");
+      Serial.print(yaw, DEC);
+      printVector(v_offsets);*/
+
+      unsigned long duration = analog_read();
+      double distance = slope * duration + offset;//compute distance
+      geo2.pitch_y = geo1.pitch_y - distance;
+      Vector v_signal;
+      calculateOffset(geo2, theta, azimuth, v_signal);
+      
+     /* Serial.print("Pitch: ");
+      Serial.print(pitch, DEC);
+      Serial.print(", Yaw: ");
+      Serial.print(yaw, DEC);
+      printVector(v_signal);*/
+      printVectorDifference(v_offsets, v_signal);
+      //printSphereToVector(distance, theta, azimuth, v);
+      //Serial.print(distance, 5); //print distacne to 5 decimal places
+      delay(20);
+      if (yaw < yend - ystep)
+      {
+        Serial.print("; ");
+      }
+
+    }
+    if (pitch < pend - pstep)
+    {
+      Serial.println("; ");
+    }
+  }
+  Serial.println("]");
+  Serial.println("");
+  delay(500000);
+}
